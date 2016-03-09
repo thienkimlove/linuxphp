0473d060ba938c4c81a59153c3342b88ff9a6d18
Quan Do <do.manh.quan@vsi-international.com>
2016-03-09 14:48:58 +0700

    first commit

--- resources/views/admin/nav.blade.php
+++ resources/views/admin/nav.blade.php
@@ -1,7 +1,7 @@
 <!-- Navigation -->
 <nav class="navbar navbar-default navbar-static-top" role="navigation" style="margin-bottom: 0">
     <div class="navbar-header">
-        <a class="navbar-brand">Admin v2.0</a>
+        <a class="navbar-brand">Content Management</a>
     </div>
     <!-- /.navbar-header -->
 
